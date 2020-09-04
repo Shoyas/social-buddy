7ab290b9a34c94cd760be5bb88180493f1ef836b
Shoyas <md.nasiruddin4067@gmail.com>
2020-09-05 00:09:47 +0600

    first commit

---
+++ src/components/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Post = (props) => {
+    const {post} = props;
+    
+    console.log(post);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Post;
